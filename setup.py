6f11e15a7d447055ca1ce24025bda6141b0d2d0c
robertgr991 <36689800+robertgr991@users.noreply.github.com>
2019-08-26 14:54:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ README = open("./README.md").read()
 
 metadata = dict(
     name='fastDamerauLevenshtein',
-    version='1.0.4',
+    version='1.0.5',
     description='Cython implementation of true Damerau-Levenshtein algorithm.',
     long_description=README,
     long_description_content_type="text/markdown",
